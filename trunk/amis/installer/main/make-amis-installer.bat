21a3624ab5edd001579f25c336759bb6dec7d4d0
Marisa DeMeglio <marisa.demeglio@gmail.com>
2008-12-09 04:39:16 +0000

    fixes #178 fixes #180

--- trunk/amis/installer/main/make-amis-installer.bat
+++ trunk/amis/installer/main/make-amis-installer.bat
@@ -1,4 +1,9 @@
-﻿REM   make-amis-installer.bat langid langname
+REM   make-amis-installer.bat langid langname helpfile
 
+ECHO Customizing preferences for %2
 perl setup-amis-prefs.pl ../../bin/settings/amisPrefs.xml.default ui-lang-id %1 > ../../bin/settings/clean_settings_for_the_installer/amisPrefs.xml 
-"C:\Program Files\NSIS\makensis" /DCUSTOM_LANG_ID=%1 /DCUSTOM_LANG_NAME=%2 setup-amis3.nsi
+
+ECHO Creating installer
+"C:\Program Files\NSIS\makensis" /DCUSTOM_LANG_ID=%1 /DCUSTOM_LANG_NAME=%2 /DCUSTOM_HELP=%3 setup-amis3.nsi
+
+ECHO Done
---
+++ trunk/amis/installer/main/make-default-amis-installer.bat
@@ -0,0 +1,2 @@
+make-amis-installer.bat "eng-US" "U.S. English" ""
+move "Setup-amis3-beta4-U.S. English.exe" "Setup-amis3-beta4-U.S.English.exe"
